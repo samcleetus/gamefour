02f86bdd5506a8dc7e6ad1e706f3296893bd154e
Sam Cleetus <iamsamcleetus@gmail.com>
2021-06-05 10:50:47 -0400

    Added new ememy and Boss

--- meanies.py
+++ meanies.py
@@ -25,6 +25,8 @@ knight_right_img = pygame.image.load("./assets/fgame/knight_right.png")
 knight_left_img = pygame.image.load("./assets/fgame/knight_left.png")
 enemy_left = pygame.image.load("./assets/fgame/zombie_left.png")
 enemy_right = pygame.image.load("./assets/fgame/zombie_right.png")
+boss_left = pygame.image.load("./assets/fgame/boss_left.png")
+boss_right = pygame.image.load("./assets/fgame/boss_right.png")
 health_box_img = pygame.image.load('./assets/img_4/icons/health_box.png')
 crosshair_img = pygame.image.load("./assets/fgame/crosshair.png")
 bg_img = pygame.image.load("./assets/fgame/main_hall.png")
@@ -46,6 +48,7 @@ green = (0, 255, 0)
 
 #groups
 enemy_group = pygame.sprite.Group()
+boss_group = pygame.sprite.Group()
 
 #game variables
 move = 0
@@ -54,6 +57,7 @@ change_dir = False
 moving_right = False
 moving_left = False
 score = 0
+game_score = 0
 font = pygame.font.SysFont('Futura', 30)
 
 #defs
@@ -73,24 +77,29 @@ def check_player_movement():
     return change_dir
 
 def get_random():
-    rand_enemies = random.randint(2, 4)
+    #rand_enemies = random.randint(2, 4)
     rand_x = random.randint(1, 800)
     rand_y = random.randint(1, 800)
-    rand_wave = random.randint(1, 15)
+    rand_gremlin = random.randint(1, 2)
+    rand_enemies = random.randint(1, 3)
 
-    return rand_enemies, rand_x, rand_y
+    return rand_x, rand_y, rand_gremlin, rand_enemies
 
 def draw_img(img, x, y):
     img = img
     screen.blit(img, (x, y))
 
+def get_boss():
+    rand_boss = random.randint(1, 20)
+
+    return rand_boss
 
 #classes
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, speed, damage):
         pygame.sprite.Sprite.__init__(self)
         self.alive = True
-        self.max_health = 10000
+        self.max_health = 1000
         self.health = self.max_health
         self.damage = damage
         self.image = pygame.transform.scale(knight_left_img, (50, 60))
@@ -134,6 +143,9 @@ class Player(pygame.sprite.Sprite):
             for enemy in enemy_group:
                 if enemy.rect.collidepoint(pos):
                     enemy.health -= self.damage
+            for boss in boss_group:
+                if boss.rect.collidepoint(pos):
+                    boss.health -= self.damage
 
     def check_alive(self):
         if self.health <= 0:
@@ -146,20 +158,25 @@ class Player(pygame.sprite.Sprite):
         for enemy in enemy_group:
             if pygame.sprite.spritecollide(self, enemy_group, False) and enemy.alive:
                 self.health -= enemy.damage
+        for boss in boss_group:
+            if pygame.sprite.spritecollide(self, boss_group, False) and boss.alive:
+                self.health -= boss.damage
 
 
     def draw(self):
         screen.blit(self.image, self.rect)
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, x, y, speed, damage):
+    def __init__(self, name, x, y, speed, damage):
         pygame.sprite.Sprite.__init__(self)
         self.alive = True
+        self.name = name
         self.speed = speed
         self.max_health = 100
         self.health = self.max_health
         self.damage = damage
-        self.image = pygame.transform.scale(enemy_left, (33, 46))
+        img = pygame.image.load(f"./assets/fgame/{self.name}_left.png")
+        self.image = pygame.transform.scale(img, (33, 46))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
@@ -175,22 +192,35 @@ class Enemy(pygame.sprite.Sprite):
 
         change_dir = check_player_movement()
         if change_dir == 1:
-            self.image = pygame.transform.scale(enemy_right, (33, 46))
+            img = pygame.image.load(f"./assets/fgame/{self.name}_right.png")
+            self.image = pygame.transform.scale(img, (33, 46))
         elif change_dir == 2:
-            self.image = pygame.transform.scale(enemy_left, (33, 46))
+            img = pygame.image.load(f"./assets/fgame/{self.name}_left.png")
+            self.image = pygame.transform.scale(img, (33, 46))
 
         # Move along this normalized vector towards the player at current speed.
         self.rect.x += dx * self.speed
         self.rect.y += dy * self.speed
 
     def spawn(self):
-        WAVE_COUNTER = 0
-        rand_enemies, rand_x, rand_y = get_random()
+        rand_x, rand_y, rand_gremlin, rand_enemies = get_random()
         if len(enemy_group) >= 4:
             rand_enemies = 0
-        for enemy in range(rand_enemies):
-            enemy = Enemy(rand_x, rand_y, 2, 1)
-            enemy_group.add(enemy)
+        if rand_gremlin == 1:
+            for enemy in range(rand_enemies):
+                rand_x, rand_y, rand_gremlin, rand_enemies = get_random()
+                enemy = Enemy('zombie', rand_x, rand_y, 2, 1)
+                enemy_group.add(enemy)
+
+        elif rand_gremlin == 2:
+            for gremlin in range(rand_enemies):
+                rand_x, rand_y, rand_boss, rand_enemies = get_random()
+                enemy = Enemy('gremlin', rand_x, rand_y, 2, 1)
+                enemy_group.add(enemy)
+
+        elif rand_gremlin == 3:
+            pass
+        
 
     def check_alive(self):
         global change_dir
@@ -221,10 +251,69 @@ class HealthBar():
         pygame.draw.rect(screen, red, (self.x, self.y, 150, 20))
         pygame.draw.rect(screen, green, (self.x, self.y, 150 * ratio, 20))
 
+class Boss(pygame.sprite.Sprite):
+    def __init__(self, x, y, speed, damage):
+        pygame.sprite.Sprite.__init__(self)
+        self.alive = True
+        self.speed = speed
+        self.max_health = 250
+        self.health = self.max_health
+        self.damage = damage
+        self.image = pygame.transform.scale(boss_left, (61, 80))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def move_towards_player(self, player):
+        # Find direction vector (dx, dy) between enemy and player.
+        dx, dy = player.rect.x - self.rect.x, player.rect.y - self.rect.y
+        dist = math.hypot(dx, dy)
+        #print(dist)
+        try:
+            dx, dy = dx / dist, dy / dist  
+        except:
+            pass
+
+        change_dir = check_player_movement()
+        if change_dir == 1:
+            self.image = pygame.transform.scale(boss_right, (61, 80))
+        elif change_dir == 2:
+            self.image = pygame.transform.scale(boss_left, (61, 80))
+
+        # Move along this normalized vector towards the player at current speed.
+        self.rect.x += dx * self.speed
+        self.rect.y += dy * self.speed
+
+    def spawn(self):
+        rand_x, rand_y, rand_bigboss, rand_enemies = get_random()
+        rand_enemies = 1
+        for enemy in range(rand_enemies):
+            #rand_x, rand_y, rand_boss = get_random()
+            boss = Boss(rand_x, rand_y, 2, 5)
+            boss_group.add(boss)
+        
+
+    def check_alive(self):
+        global change_dir
+        if self.health <= 0:
+            self.health = 0
+            self.speed = 0
+            self.damage = 0
+            self.alive = False
+
+    def draw(self):
+        screen.blit(self.image, self.rect)
+
+#class Potion():
+    #def __init__(self, name, x, y):
+        #self.name = name
+        
+
 #create instances
 player = Player(400, 400, 5, 50)
-enemy = Enemy(600, 200, 2, 1)
+enemy = Enemy('zombie', 600, 200, 2, 1)
 enemy_group.add(enemy)
+boss = Boss(600, 200, 2, 5)
+boss_group.add(boss)
 
 health_bar = HealthBar(52, int(18.5), player.health, player.max_health)
 
@@ -254,12 +343,24 @@ while run:
         enemy.draw()
         enemy.check_alive()
         enemy.update()
+    if score > 5:
+        boss.draw()
+        boss.move_towards_player(player)
+        boss.check_alive()
+        boss.update()
 
     for enemy in enemy_group:
         if enemy.alive == False:
             score += 1
             enemy.spawn()
             enemy.kill()
+
+    for boss in boss_group:
+        if boss.alive == False:
+            score += 5
+            boss.spawn()
+            boss.kill()
+
 
     #change mouse image
     #screen.blit( MANUAL_CURSOR, ( pygame.mouse.get_pos() ) )
